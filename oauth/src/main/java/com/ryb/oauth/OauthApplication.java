415d678d0f0a80ad08c545546f1010049b6a05ae
1063730142@qq.com <1063730142@qq.com>
2020-05-09 17:34:09 +0800

    代码调整

--- oauth/src/main/java/com/ryb/oauth/OauthApplication.java
+++ oauth/src/main/java/com/ryb/oauth/OauthApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 常坤
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @ServletComponentScan
 @MapperScan("com.ryb.oauth")
+@EnableFeignClients
 public class OauthApplication {
 
     public static void main(String[] args) {
